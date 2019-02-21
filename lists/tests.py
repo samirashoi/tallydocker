416cf6b8658aea67e2ba7df0bc24130c80c01aa2
samirashoi <issamrasheed@gmail.com>
2019-02-21 21:18:01 +0530

    Remove unused imports

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 from lists.views import home_page
 # from django.template.loader import render_to_string
 
@@ -9,4 +7,3 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')                 # Django Test Client
         self.assertTemplateUsed(response, 'home.html')  # will only work for response objects from Django Test Client
-        
