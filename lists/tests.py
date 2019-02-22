cb447d3ce3a094969aa7b45f747059f3eb923abf
samirashoi <issamrasheed@gmail.com>
2019-02-23 01:09:59 +0530

    Add model Item to lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.views import home_page
+from lists.models import Item
 # from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
@@ -12,3 +13,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text':'Tis a new item'})
         self.assertIn('Tis a new item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+        second_item = Item()
+        second_item.text = "Second Item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Second Item')
