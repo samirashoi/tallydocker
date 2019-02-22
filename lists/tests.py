da3ecbc55fa86ec5df0e862ff02f579d47eec8cb
samirashoi <issamrasheed@gmail.com>
2019-02-23 00:30:15 +0530

    Update FT and add table to HTML

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')                 # Django Test Client
         self.assertTemplateUsed(response, 'home.html')  # will only work for response objects from Django Test Client
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'Tis a new item'})
+        self.assertIn('Tis a new item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
