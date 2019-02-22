4e9251b1d2ba8940b254fe81f9a52e4663fae1e7
samirashoi <issamrasheed@gmail.com>
2019-02-23 01:54:55 +0530

    Implement redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -10,10 +10,28 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')  # will only work for response objects from Django Test Client
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text':'Tis a new item'})
-        self.assertIn('Tis a new item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={'item_text':'Tis a new item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Tis a new item')
 
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text':'Tis a new item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'],'/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
@@ -32,3 +50,4 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Second Item')
+    
